94a3703b54f2fb1cdd08842aab1894ef798704bb
Sean Carpenter <sean@seancarpenter.net>
2014-10-28 19:28:40 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,77 @@
+var Master = React.createClass({
+  initialData: [
+    { detailId: 1, speed: 25, direction: "South" },
+    { detailId: 2, speed: 25, direction: "South" },
+    { detailId: 3, speed: 25, direction: "South" }
+  ],
+
+  getInitialState: function() {
+    return { displays: this.initialData };
+  },
+
+  updateData: function() {
+    var self = this;
+    var newData = this.initialData.map(function(row) {
+      return { detailId: row.detailId, speed: self.refs.speed.getDOMNode().value, direction: self.refs.direction.getDOMNode().value };
+    });
+    this.setState({ displays: newData });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <div className="master">
+          <p>
+            <label htmlFor="speed">Speed:</label>
+            <input type="text" id="speed" name="speed" defaultValue="25" ref="speed" />
+          </p>
+          <p>
+            <label htmlFor="direction">Direction:</label>
+            <input type="text" id="direction" name="direction" defaultValue="South" ref="direction" />
+          </p>
+        </div>
+        <button type="button" onClick={this.updateData}>Update!</button>
+        <DetailList displays={this.state.displays}/>
+      </div>
+    );
+  }
+});
+
+var DetailList = React.createClass({
+  render: function() {
+    var rows = this.props.displays.map(function(row){
+      return <DetailRow detailId={row.detailId} speed={row.speed} direction={row.direction} />;
+    });
+    return (
+      <table className="detail-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Speed</th>
+            <th>Direction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+});
+
+var DetailRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.detailId}</td>
+        <td>{this.props.speed}</td>
+        <td>{this.props.direction}</td>
+      </tr>
+    );
+  }
+});
+
+React.render(
+  <Master/>,
+  document.getElementById("content")
+);
